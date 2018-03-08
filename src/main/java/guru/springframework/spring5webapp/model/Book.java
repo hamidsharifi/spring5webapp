b64b5632bc2907ad001e68d04d38ddba1f50f1f7
Hamid Sharifi <HamidSharifi@Levo.com.au>
2018-03-08 16:36:31 +1100

    Adding Controller

--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -14,6 +14,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    
+    @OneToOne
+    private Publisher publisher;
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
@@ -23,10 +26,12 @@ public class Book {
     public Book() {
     }
 
-    public Book(String title, String isbn) {
-        this.title = title;
-        this.isbn = isbn;
-    }
+    public Book(String title, String isbn, Publisher publisher) {
+		super();
+		this.title = title;
+		this.isbn = isbn;
+		this.publisher = publisher;
+	}
 
     public Book(String title, String isbn, Set<Author> authors) {
         this.title = title;
@@ -90,4 +95,12 @@ public class Book {
                 ", authors=" + authors +
                 '}';
     }
+
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
 }
